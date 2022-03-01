3f6719f702a504cee98e07e4e7ca08367520a430
Ryan Nissen <ryankn852@gmail.com>
2022-03-01 14:01:04 -0600

    Fixed text to center in circle. Moved EightBall answers to their own file

--- src/EightBall.js
+++ src/EightBall.js
@@ -7,10 +7,14 @@ function EightBall({ answers }) {
   const [color, setColor] = useState("black");
   const [message, setMessage] = useState("Think of a Question.");
 
+  const eightBallStyles = {"background-color": color}
+
   return (
     <div className="EightBall">
-      <span className="EightBall-dot">
-
+      <span className="EightBall-dot" style={eightBallStyles}>
+        <div className="EightBall-message">
+          {message}
+        </div>
       </span>
     </div>
   );
