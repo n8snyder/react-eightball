1b99e035e8efc6e430bde07a1a9e91a4c0adfe9f
Nate Snyder <n8.snyder@berkeley.edu>
2022-03-01 12:35:42 -0800

    Wrote callback to pick answer and update state

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,17 +1,40 @@
 import { useState } from "react";
 
 import './EightBall.css';
+import defaultAnswers from './EightBallAnswers';
 
-function EightBall({ answers }) {
+// TODO: non component files should not be uppercase, only components should
+// TODO: Could move defaultAnswers to this file and export.
+
+/** Magic eight ball game
+ * 
+ * Props:
+ * - answers: [{ msg: "It is certain.", color: "green" }, ...]
+ * 
+ * State:
+ * - color
+ * - message
+ * 
+ * App -> EightBall
+ */
+
+function EightBall({ answers = { defaultAnswers } }) {
 
   const [color, setColor] = useState("black");
   const [message, setMessage] = useState("Think of a Question.");
 
-  const eightBallStyles = {"background-color": color}
+  /** handlePickAnswer: Picks random answer and updates color and message */
+  function handlePickAnswer() {
+    const randomIdx = Math.floor(Math.random() * answers.length);
+    const answer = answers[randomIdx];
+    setColor(answer.color);
+    setMessage(answer.msg);
+  }
 
+  const eightBallStyles = { backgroundColor: color }
   return (
     <div className="EightBall">
-      <span className="EightBall-dot" style={eightBallStyles}>
+      <span className="EightBall-dot" style={eightBallStyles} onClick={handlePickAnswer}>
         <div className="EightBall-message">
           {message}
         </div>
